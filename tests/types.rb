d437b47562e630352b17cae2f0245bf6bbe64baf
loki2302 <loki2302@loki2302.me>
2013-07-28 01:05:07 +0400

    More tests

--- tests/types.rb
+++ tests/types.rb
@@ -7,5 +7,6 @@ class Types < Test::Unit::TestCase
     assert_equal(Float, 3.14.class)
     assert_equal(Array, [1, 2, 3].class)
     assert_equal(Hash, {}.class)
+    assert_equal(NilClass, nil.class)
   end
 end
