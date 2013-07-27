83336a294096622cd28b6f93a96e4dc0ebfd9b44
loki2302 <loki2302@loki2302.me>
2013-07-27 22:51:21 +0400

    Initial version

---
+++ tests/test1.rb
@@ -0,0 +1,7 @@
+require "test/unit"
+
+class TestStuff1 < Test::Unit::TestCase
+    def test_stuff1
+	assert_equal(3, 1+2)
+    end
+end
---
+++ tests/test2.rb
@@ -0,0 +1,7 @@
+require "test/unit"
+
+class TestStuff2 < Test::Unit::TestCase
+    def test_stuff2
+	assert_equal(3, 1+2)
+    end
+end
