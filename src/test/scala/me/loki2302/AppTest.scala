574796bdb7bd53d52a10d5ccaa4bc0dbf3b71f03
unknown <loki2302@loki2302.me>
2013-07-28 08:11:42 +0400

    Added test

--- src/test/scala/me/loki2302/AppTest.scala
+++ src/test/scala/me/loki2302/AppTest.scala
@@ -4,6 +4,16 @@ import org.scalatest.FunSuite
 
 class AppTest extends FunSuite {
   test("one plus two is three") {
-    assert(1 + 2 == 3);
-  }  
+    assert(1 + 2 == 3)
+  }
+
+  test("can use objects") {
+    object Calculator {
+      def add(a: Int, b: Int): Int = {
+        return a + b
+      }
+    }
+
+    assert(Calculator.add(1, 2) == 3)
+  }
 }
