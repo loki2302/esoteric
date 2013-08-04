2b0a92aa28a6c63f0835612ebeb31037c6d1ef70
loki2302 <loki2302@loki2302.me>
2013-08-04 10:43:40 +0400

    Calculator#add now can return both HTML and JSON based on .json suffix presence

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -1,6 +1,11 @@
 class CalculatorController < ApplicationController
   def add_numbers
     @result = params[:a].to_i + params[:b].to_i
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => {:result => @result} }
+    end
   end
 
   def add_numbers_json
