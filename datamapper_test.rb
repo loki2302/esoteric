c947861578fd5cf1bd1f9e7e611fc6417e2785a2
loki2302 <loki2302@loki2302.me>
2013-08-06 00:23:43 +0400

    Added more DataMapper tests

--- datamapper_test.rb
+++ datamapper_test.rb
@@ -3,12 +3,15 @@ require "data_mapper"
 require "dm-migrations"
 
 class DataMapperTest < Test::Unit::TestCase
-  def test_datamapper_even_works
+  def setup    
     DataMapper::Logger.new($stdout, :debug)
     DataMapper.setup(:default, "sqlite::memory:")
     DataMapper.auto_migrate!
+  end
 
+  def test_datamapper_even_works
     user = User.new(:name => "loki2302")
+    assert_nil(user.id)
     user.save!
     assert_equal(1, user.id)
 
@@ -35,6 +38,31 @@ class DataMapperTest < Test::Unit::TestCase
 
     users = User.all
     assert_equal(1, users.count)
+    assert_equal(1, User.count)
+  end
+
+  def test_can_update_user
+    user = User.new(:name => "loki2302")
+    user.save!
+    assert_not_nil(user.id)
+
+    user_id = user.id
+    user = User.get(user_id)
+    assert_equal("loki2302", user.name)
+
+    user.update(:name => "not loki2302")
+
+    user = User.get(user_id)
+    assert_equal("not loki2302", user.name)
+  end
+
+  def test_can_delete_use
+    user = User.new(:name => "loki2302")
+    user.save!
+    assert_equal(1, User.count)
+
+    user.destroy
+    assert_equal(0, User.count)
   end
 end
 
