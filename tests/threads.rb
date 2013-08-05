38b8cffbe53fc6ff223201e504bc5908edb824ad
loki2302 <loki2302@loki2302.me>
2013-08-06 00:44:06 +0400

    Added one more test

--- tests/threads.rb
+++ tests/threads.rb
@@ -10,4 +10,13 @@ class ThreadsTest < Test::Unit::TestCase
     assert_equal(1, x.count)
     assert_equal(123, x[0])
   end
+
+  def test_can_return_value_from_thread
+    t = Thread.new do
+      sleep 0.2
+      123
+    end
+
+    assert_equal(123, t.value)
+  end
 end
