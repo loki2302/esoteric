e65f5f497eaf69460290538b1b70f1549e1fff53
loki2302 <loki2302@loki2302.me>
2013-08-07 01:27:15 +0400

    Refactoring

--- datamapper_multiple_entities_test.rb
+++ datamapper_multiple_entities_test.rb
@@ -10,28 +10,28 @@ class DataMapperMultipleEntitiesTest < Test::Unit::TestCase
   end
 
   def test_can_use_multiple_entities
-    user = User2.new(:name => "loki2302")
+    user = PostingUser.new(:name => "loki2302")
     user.save!
-    assert_equal(1, User2.count)
+    assert_equal(1, PostingUser.count)
     assert_equal(0, Post.count)
 
-    post1 = Post.new(:text => "first post", :user2 => user)
+    post1 = Post.new(:text => "first post", :posting_user => user)
     post1.save!
-    assert_equal(1, User2.count)
+    assert_equal(1, PostingUser.count)
     assert_equal(1, Post.count)
 
     post1 = Post.first
     assert_equal("first post", post1.text)
-    assert_equal(1, post1.user2.id)
-    assert_equal("loki2302", post1.user2.name)
+    assert_equal(1, post1.posting_user.id)
+    assert_equal("loki2302", post1.posting_user.name)
 
-    user = User2.first
+    user = PostingUser.first
     assert_equal(1, user.posts.count)
     assert_equal(1, user.posts.first.id)
   end
 end
 
-class User2
+class PostingUser
   include DataMapper::Resource
 
   property :id, Serial
@@ -44,7 +44,7 @@ class Post
 
   property :id, Serial
   property :text, String
-  belongs_to :user2
+  belongs_to :posting_user
 end
 
 DataMapper.finalize
