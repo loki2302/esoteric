25218bfadb8286943ca877aaa956e30f587cf8e5
loki2302 <loki2302@loki2302.me>
2013-08-06 13:34:21 +0400

    Minor progress

--- app.rb
+++ app.rb
@@ -1,16 +1,19 @@
 require "nokogiri"
 require "open-uri"
 
-#page_uri = "http://www.nhl.com/ice/playersearch.htm"
-#doc = Nokogiri::HTML(open(page_uri))
-#doc.css("#playerSearch > .lastInitial > a").each do |link|
-#  letter_uri = URI.join(page_uri, link["href"]).to_s
-#  puts "got link: #{letter_uri}"
-#end
-
 def main
-  task = ProcessLettersTask.new("http://www.nhl.com/ice/playersearch.htm")
-  puts task.run
+  first_task = ProcessLettersTask.new("http://www.nhl.com/ice/playersearch.htm")
+  context = CrawlerContext.new
+  context.submit_task(first_task)
+  loop do
+    task = context.get_task
+    if task == nil
+      break
+    end
+
+    puts "Running #{task}"
+    task.run(context)
+  end
 end
 
 class ProcessLettersTask
@@ -18,11 +21,45 @@ class ProcessLettersTask
     @uri = uri
   end
 
-  def run
+  def run(context)
     doc = Nokogiri::HTML(open(@uri))
-    doc.css("#playerSearch > .lastInitial > a").map do |link|
-      URI.join(@uri, link["href"]).to_s
+    doc.css("#playerSearch > .lastInitial > a").each do |link|
+      letter_uri = URI.join(@uri, link["href"]).to_s
+      task = ProcessLetterTask.new(letter_uri, true)
+      context.submit_task(task)
     end
+  end
+
+  def to_s
+    "ProcessLettersTask{#{@uri}}"
+  end
+end
+
+class ProcessLetterTask
+  def initialize(uri, process_pagination)
+    @uri = uri
+    @process_pagination = process_pagination
+  end
+
+  def run(context)
+  end
+
+  def to_s
+    "ProcessLetterTask{#{@uri}}"
+  end
+end
+
+class CrawlerContext
+  def initialize
+    @tasks = Queue.new
+  end
+
+  def submit_task(task)
+    @tasks.push(task)
+  end
+
+  def get_task()
+    @tasks.pop(true) rescue nil
   end
 end
 
