f5c0aa92cb821b1b1dfe72aa44852edf364fe15c
loki2302 <loki2302@loki2302.me>
2014-09-11 11:23:08 +0400

    Added formatting interpolation test

--- src/test/scala/me/loki2302/StringInterpolationTest.scala
+++ src/test/scala/me/loki2302/StringInterpolationTest.scala
@@ -11,4 +11,12 @@ class StringInterpolationTest {
     val message = s"hello, $username!"
     assertEquals("hello, loki2302!", message)
   }
+
+  @Test
+  def canUseFormattingInterpolation(): Unit = {
+    val secret = 2302
+    val username = f"loki$secret%d"
+    val message = f"hello, $username%s!"
+    assertEquals("hello, loki2302!", message)
+  }
 }
