7244bf4ae53c404b3f4c7f5d1cd402e796819c0f
loki2302 <loki2302@loki2302.me>
2014-09-11 11:48:05 +0400

    Added another method to ImplicitCalculator

--- src/test/scala/me/loki2302/ImplicitClassTest.scala
+++ src/test/scala/me/loki2302/ImplicitClassTest.scala
@@ -6,11 +6,15 @@ import org.junit.Assert.assertEquals
 class ImplicitClassTest {
   @Test
   def canUseImplicitCalculator(): Unit = {
-    val result : Int = 2 add 3
-    assertEquals(5, result)
+    val addResult: Int = 2 add 3
+    assertEquals(5, addResult)
+
+    val subResult: Int = 2 sub 3
+    assertEquals(-1, subResult)
   }
 
   implicit class ImplicitCalculator(val x: Int) {
     def add(y: Int): Int = x + y
+    def sub(y: Int): Int = x - y
   }
 }
