7286fc816c1b4b5e85a94f5e53e596746e647703
loki2302 <loki2302@loki2302.me>
2014-10-27 11:06:22 +0300

    Added canUseASTTransformation

--- src/test/groovy/me/loki2302/meta/MetaprogrammingTest.groovy
+++ src/test/groovy/me/loki2302/meta/MetaprogrammingTest.groovy
@@ -1,7 +1,10 @@
 package me.loki2302.meta
 
+import org.codehaus.groovy.control.CompilePhase
+import org.codehaus.groovy.tools.ast.TransformTestHelper
 import org.junit.Test
-import static org.junit.Assert.*
+
+import static org.junit.Assert.assertEquals
 
 public class MetaprogrammingTest {
     @Test
@@ -18,5 +21,32 @@ public class MetaprogrammingTest {
 
         assertEquals 123, (int)anyProperty.x
         assertEquals 'qwerty', anyProperty.s
+    }
+
+    @Test
+    public void canUseASTTransformation() {
+        def transformTestHelper = new TransformTestHelper(
+                new PrependPrintlnASTTransformation(),
+                CompilePhase.SEMANTIC_ANALYSIS);
+
+        def dummyClass = transformTestHelper.parse('''
+
+class Dummy {
+    @me.loki2302.meta.PrependPrintln
+    public void hello() {
+        println 'I am hello method'
+    }
+}
+
+''')
+
+        def dummy = dummyClass.newInstance()
+        dummy.hello()
+
+        // TODO: how do I test it actually?
+        // Says:
+        // Applying AST transformation! (from PrependPrintlnASTTransformation)
+        // I am AST transformation (from code added by PrependPrintlnASTTransformation)
+        // I am hello method (from Dummy)
     }
 }
