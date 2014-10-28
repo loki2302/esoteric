46e0d3d517e4a39cef205a82443baefd012f8623
loki2302 <loki2302@loki2302.me>
2014-10-28 09:06:46 +0300

    Added ToStringTransformationTest

---
+++ src/test/groovy/me/loki2302/ToStringTransformationTest.groovy
@@ -0,0 +1,21 @@
+package me.loki2302
+
+import groovy.transform.ToString
+import org.junit.Test
+
+import static org.junit.Assert.*;
+
+public class ToStringTransformationTest {
+    @Test
+    public void canUseToStringTransformation() {
+        def person = new PersonWithSimpleToString(id: 123, name: 'loki2302')
+        String s = person.toString()
+        assertEquals ToStringTransformationTest.class.getName() + '$PersonWithSimpleToString(123, loki2302)', s
+    }
+
+    @ToString
+    public static class PersonWithSimpleToString {
+        int id;
+        String name;
+    }
+}
