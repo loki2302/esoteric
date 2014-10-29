158c219c2999718631dbc2c6476e91a7523ad850
loki2302 <loki2302@loki2302.me>
2014-10-29 08:46:52 +0300

    Added more tests to ListsTest

--- src/test/groovy/me/loki2302/ListsTest.groovy
+++ src/test/groovy/me/loki2302/ListsTest.groovy
@@ -18,15 +18,42 @@ class ListsTest {
 
         assertEquals 4, list.last()
         assertEquals 4, list[2]
+    }
 
-        list.add(123)
-        assertEquals 4, list.size()
-        assertEquals 123, list.last()
-        assertEquals 123, list[3]
+    @Test
+    void canAddElementToList() {
+        def list = [11, 22, 33, 44]
+        list << 55
+        assertEquals 5, list.size()
+        assertEquals 55, list.last()
+    }
 
-        def sublist = list[1..3]
-        assertEquals 3, sublist.size()
-        assertEquals 3, sublist.first()
-        assertEquals 123, sublist.last()
+    @Test
+    void canGetASubList() {
+        def list = [11, 22, 33, 44, 55]
+        def subList = list[1..3]
+        assertEquals 3, subList.size()
+        assertEquals 22, subList[0]
+        assertEquals 33, subList[1]
+        assertEquals 44, subList[2]
+    }
+
+    @Test
+    void canMapList() {
+        def list = [11, 22, 33, 44, 55]
+        def newList = list.collect { 2 * it }
+        assertEquals 5, newList.size()
+        assertEquals 2 * 11, newList[0]
+        assertEquals 2 * 22, newList[1]
+        assertEquals 2 * 33, newList[2]
+        assertEquals 2 * 44, newList[3]
+        assertEquals 2 * 55, newList[4]
+    }
+
+    @Test
+    void canReduceList() {
+        def list = [11, 22, 33, 44]
+        def sum = list.inject(0, { acc, val -> acc + val })
+        assertEquals 11 + 22 + 33 + 44, sum
     }
 }
