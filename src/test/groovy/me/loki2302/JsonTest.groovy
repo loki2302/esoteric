2a528505147e153b1fa719a59b3116277cfe2fea
loki2302 <loki2302@loki2302.me>
2014-11-02 13:33:03 +0300

    Added canWriteJson test

--- src/test/groovy/me/loki2302/JsonTest.groovy
+++ src/test/groovy/me/loki2302/JsonTest.groovy
@@ -1,9 +1,10 @@
 package me.loki2302
-
+import groovy.json.JsonBuilder
 import groovy.json.JsonSlurper
 import org.junit.Test
 
-import static org.junit.Assert.*
+import static org.junit.Assert.assertEquals
+import static org.junit.Assert.assertTrue
 
 class JsonTest {
     @Test
@@ -23,5 +24,24 @@ class JsonTest {
         assertTrue obj.interests instanceof List
         assertEquals 'java', obj.interests[0]
         assertEquals 'groovy', obj.interests[1]
+    }
+
+    @Test
+    void canWriteJson() {
+        def builder = new JsonBuilder()
+        def root = builder {
+            name 'loki2302'
+            age 30
+            interests 'java', 'groovy'
+        }
+
+        def json = builder.toString()
+        assertEquals '{"name":"loki2302","age":30,"interests":["java","groovy"]}', json
+    }
+
+    static class Person {
+        def name
+        def age
+        def interests
     }
 }
