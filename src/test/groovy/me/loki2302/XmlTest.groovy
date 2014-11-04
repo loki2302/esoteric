77dd4448824d3188179f3bd2b7ed3e7268e247b9
loki2302 <loki2302@loki2302.me>
2014-11-04 09:54:49 +0300

    Added MarkupBuilder test

--- src/test/groovy/me/loki2302/XmlTest.groovy
+++ src/test/groovy/me/loki2302/XmlTest.groovy
@@ -1,10 +1,11 @@
 package me.loki2302
 
+import groovy.xml.MarkupBuilder
 import org.junit.Test
 
 import static org.junit.Assert.*
 
-class XmlSlurperTest {
+class XmlTest {
     @Test
     void canParseXml() {
         def xmlSlurper = new XmlSlurper()
@@ -24,5 +25,30 @@ class XmlSlurperTest {
         assertEquals 2, obj.interests.interest.size()
         assertEquals 'java', obj.interests.interest[0].text()
         assertEquals 'groovy', obj.interests.interest[1].text()
+    }
+
+    @Test
+    void canWriteXml() {
+        def stringWriter = new StringWriter()
+        def builder = new MarkupBuilder(stringWriter)
+        builder.setDoubleQuotes(true)
+        builder.person(age: 30) {
+            name 'loki2302'
+            interests {
+                interest 'java'
+                interest 'groovy'
+            }
+        }
+
+        def xml = stringWriter.toString()
+        assertEquals '''\
+<person age="30">
+  <name>loki2302</name>
+  <interests>
+    <interest>java</interest>
+    <interest>groovy</interest>
+  </interests>
+</person>\
+''', xml
     }
 }
