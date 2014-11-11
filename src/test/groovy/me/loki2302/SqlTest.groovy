8c5f624c968202162a4778f29bc6d06fcb6e41dc
loki2302 <loki2302@loki2302.me>
2014-11-11 10:49:49 +0300

    Added more SQL tests

--- src/test/groovy/me/loki2302/SqlTest.groovy
+++ src/test/groovy/me/loki2302/SqlTest.groovy
@@ -1,28 +1,91 @@
 package me.loki2302
-
 import groovy.sql.Sql
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 
-import static org.junit.Assert.*
+import static org.junit.Assert.assertEquals
+import static org.junit.Assert.fail
 
 class SqlTest {
+    Sql sql
+
+    @Before
+    void setUpSql() {
+        def databaseName = UUID.randomUUID().toString()
+        sql = Sql.newInstance("jdbc:hsqldb:mem:$databaseName", 'sa', '', 'org.hsqldb.jdbc.JDBCDriver')
+    }
+
+    @After
+    void destroySql() {
+        sql.close()
+    }
+
     @Test
     void canUseSql() {
-        def sql = Sql.newInstance('jdbc:hsqldb:mem:test', 'sa', '', 'org.hsqldb.jdbc.JDBCDriver')
         sql.execute '''
 create table Notes(
-    id integer not null,
+    id int identity primary key,
     content varchar(100) not null)
 '''
-        def id = 1
-        def content = 'hello there'
-        sql.execute "insert into Notes(id, content) values($id, $content)"
+        (1..10).each {
+            def content = 'hello there ' + it
+            sql.execute "insert into Notes(content) values($content)"
+        }
 
-        def rows = sql.rows 'select id, content from Notes'
-        assertEquals 1, rows.size()
+        def notes = sql.rows 'select id, content from Notes'
+        assertEquals 10, notes.size()
 
-        def firstRow = rows.first()
-        assertEquals 1, firstRow.id
-        assertEquals 'hello there', firstRow.content
+        def firstNote = notes.first()
+        assertEquals 0, firstNote.id
+        assertEquals 'hello there 1', firstNote.content
+    }
+
+    @Test
+    void canInsertAndGetGeneratedKey() {
+        sql.execute '''
+create table Notes(
+    id int identity primary key,
+    content varchar(100) not null)
+'''
+
+        def noteId = sql.executeInsert('''
+insert into Notes(content)
+values(?)
+''', ['hello there']).first().first()
+
+        def note = sql.firstRow("select id, content from Notes where id = $noteId")
+        assertEquals noteId, note.id
+        assertEquals 'hello there', note.content
+    }
+
+    @Test
+    void canUseTransactions() {
+        sql.execute '''
+create table Notes(
+    id int identity primary key,
+    content varchar(100) not null)
+'''
+
+        try {
+            sql.withTransaction {
+                sql.executeInsert('''
+insert into Notes(content)
+values(?)
+''', ['hello there']).first().first()
+
+                def noteCount = sql.firstRow('select count(id) as noteCount from Notes').noteCount
+                assertEquals 1, noteCount
+
+                throw new RuntimeException('rollback')
+            }
+
+            fail()
+        } catch (RuntimeException e) {
+            assertEquals 'rollback', e.message
+
+            def noteCount = sql.firstRow('select count(id) as noteCount from Notes').noteCount
+            assertEquals 0, noteCount
+        }
     }
 }
