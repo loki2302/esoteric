a9a86a986f55fbd9a450c3b53aebde70b5ff3d48
loki2302 <loki2302@loki2302.me>
2014-12-28 15:40:55 +0300

    Added JSON serialization/deserialization tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+extern crate serialize;
+use serialize::json;
+
 fn main() {
   let x: int = 123;
   println!("Hello World! {}", x);
@@ -14,3 +17,29 @@ mod variable_binding_tests;
 mod function_tests;
 mod if_tests;
 mod looping_tests;
+
+// TODO: how do I move into another .rs together with extern crate thing?
+#[deriving(Encodable, Decodable)]
+struct Note {
+  id: int,
+  content: String  
+}
+
+#[test]
+fn can_serialize_to_json() {
+  let note = Note {
+    id: 123,
+    content: "hi there".to_string()
+  };
+
+  let json_string = json::encode(&note);
+  assert_eq!("{\"id\":123,\"content\":\"hi there\"}", json_string);
+}
+
+#[test]
+fn can_deserialize_from_json() {
+  let json_string = "{\"id\":123,\"content\":\"hi there\"}";
+  let note: Note = json::decode(json_string).unwrap();
+  assert_eq!(123, note.id);
+  assert_eq!("hi there", note.content);
+}
