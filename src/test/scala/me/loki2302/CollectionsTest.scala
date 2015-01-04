df473d6a00529f2a96f7eed0ac50d6041d8ebba1
loki2302 <loki2302@loki2302.me>
2015-01-04 23:30:06 +0300

    Updated dependencies, added CollectionsTest

---
+++ src/test/scala/me/loki2302/CollectionsTest.scala
@@ -0,0 +1,32 @@
+package me.loki2302
+
+import org.junit.Test;
+import org.junit.Assert._;
+
+class CollectionsTest {
+  @Test
+  def canDefineList(): Unit = {
+    val list = List(11, 22, 33)
+    assertEquals(11, list(0))
+    assertEquals(22, list(1))
+    assertEquals(33, list(2))
+  }
+
+  @Test
+  def canMapList(): Unit = {
+    val list = List(11, 22, 33).map(_ * 2)
+    assertEquals(List(22, 44, 66), list)
+  }
+
+  @Test
+  def canReduceList(): Unit = {
+    val sum = List(11, 22, 33).reduce((s, v) => s + v)
+    assertEquals(66, sum)
+  }
+
+  @Test
+  def canFilterList(): Unit = {
+    val list = List(11, 22, 33, 44).filter(_ % 2 == 0)
+    assertEquals(List(22, 44), list)
+  }
+}
