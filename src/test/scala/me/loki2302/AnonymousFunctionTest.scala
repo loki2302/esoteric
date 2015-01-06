d8f0dbff03b147251712fa3f8094469ce94c9f86
loki2302 <loki2302@loki2302.me>
2015-01-06 23:04:02 +0300

    Progress with AnonymousFunctionTest

--- src/test/scala/me/loki2302/AnonymousFunctionTest.scala
+++ src/test/scala/me/loki2302/AnonymousFunctionTest.scala
@@ -17,4 +17,29 @@ class AnonymousFunctionTest {
     val result = addOne(2)
     assertEquals(3, result)
   }
+
+  @Test
+  def canComposeFunctions(): Unit = {
+    val addOne = (x: Int) => x + 1
+    val mulByTwo = (x: Int) => x * 2
+
+    val mulByTwoAndThenAddOne = addOne.compose(mulByTwo)
+    assertEquals(3, mulByTwoAndThenAddOne(1)) // ((x * 2) + 1)
+
+    val addOneAndThenMulByTwo = addOne.andThen(mulByTwo)
+    assertEquals(4, addOneAndThenMulByTwo(1)) // ((x + 1) * 2)
+  }
+
+  @Test
+  def canUseCurrying(): Unit = {
+    val add = (x: Int, y: Int) => x + y
+    val addOne = add.curried(1)
+    assertEquals(3, addOne(2))
+  }
+
+  @Test
+  def canSupplyArgumentsAsTuple(): Unit = {
+    val add = (x: Int, y: Int) => x + y
+    assertEquals(3, add.tupled((1, 2)))
+  }
 }
