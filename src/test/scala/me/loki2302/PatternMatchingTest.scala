2ae3ae3c930a01904657f4a378abcdf197d4b3fe
loki2302 <loki2302@loki2302.me>
2015-01-06 20:47:36 +0300

    Added PatternMatchingTest

---
+++ src/test/scala/me/loki2302/PatternMatchingTest.scala
@@ -0,0 +1,31 @@
+package me.loki2302
+
+import org.junit.Test
+import org.junit.Assert._
+
+class PatternMatchingTest {
+  @Test
+  def canCompareCaseClassObjects(): Unit = {
+    assertEquals(Variable("a"), Variable("a"))
+    assertNotEquals(Variable("a"), Variable("b"))
+    assertNotEquals(Variable("a"), IntLiteral(123))
+  }
+
+  @Test
+  def canUsePatternMatchingWithCaseClasses(): Unit = {
+    assertEquals("variable-a", describe(Variable("a")));
+    assertEquals("intliteral-123", describe(IntLiteral(123)));
+    assertEquals("variable-a-plus-intliteral-123", describe(Add(Variable("a"), IntLiteral(123))));
+  }
+
+  abstract class Expr
+  case class Variable(name: String) extends Expr
+  case class IntLiteral(value: Int) extends Expr
+  case class Add(left: Expr, right: Expr) extends Expr
+
+  private def describe(expr: Expr): String = expr match {
+    case Variable(name) => "variable-" + name
+    case IntLiteral(value) => "intliteral-" + value
+    case Add(left, right) => describe(left) + "-plus-" + describe(right)
+  }
+}
