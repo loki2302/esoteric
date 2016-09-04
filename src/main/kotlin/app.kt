f60f13e11d7d3693b70b982589f2f4898569873f
loki2302 <loki2302@loki2302.me>
2016-09-04 00:21:49 -0400

    Added a few more lines

--- src/main/kotlin/app.kt
+++ src/main/kotlin/app.kt
@@ -7,4 +7,19 @@ fun main(args: Array<String>) {
 
     val p = Person("John", "Smith")
     System.out.println(p)
+
+    val (fn, ln) = p
+    System.out.println(fn)
+    System.out.println(ln)
+
+    val p2: Person
+    p2 = p
+    printPerson(p2)
+
+    val items: IntArray = intArrayOf(1, 2, 3, 4, 5)
+    System.out.println(items
+            .filter { it % 2 == 0 }
+            .map { 2 * it })
 }
+
+fun printPerson(p: Person) = System.out.println(p)
